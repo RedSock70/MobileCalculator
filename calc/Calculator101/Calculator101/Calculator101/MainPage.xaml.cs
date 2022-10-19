ed7253774d402eef36fdaf396621a9854aa59529
RedSock70 <79953666+RedSock70@users.noreply.github.com>
2022-10-19 21:05:18 +1300

    fin code
    project fin code

--- calc/Calculator101/Calculator101/Calculator101/MainPage.xaml.cs
+++ calc/Calculator101/Calculator101/Calculator101/MainPage.xaml.cs
@@ -4,15 +4,56 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using org.mariuszgromada.math.mxparser;
 
 namespace Calculator101
 {
     public partial class MainPage : ContentPage
     {
+        
+
         public MainPage()
         {
-            InitializeComponent();
+            {
+                InitializeComponent();
+               
+            } 
+
+
+            
         }
+
+        void ClickyNumber(object sender, EventArgs e)
+        {
+            Button D = (Button)sender;
+            DisplayText.Text += D.Text;
+
+        }
+
+        void ClickySign(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            DisplayText.Text += B.Text;
+
+        }
+        void ClickyEqual(object sender, EventArgs e)
+        {
+            Expression a = new Expression(DisplayText.Text);
+            DisplayText.Text = a.calculate().ToString();
+        }
+
+        void ClickyClear(object sender, EventArgs e)
+      
+        {
+            if (!string.IsNullOrEmpty(DisplayText.Text))
+            {
+                DisplayText.Text = string.Empty;
+            }
+
+             
+        }
+
     }
 }
